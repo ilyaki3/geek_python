7d119ab915d6e5a570c71899031a470e0f840394
ilyaki3 <aliwe2@yandex.ru>
2020-12-18 02:08:55 +0300

    done

--- lesson_3/les3_2.py
+++ lesson_3/les3_2.py
@@ -5,3 +5,17 @@
 параметры как именованные аргументы. Реализовать вывод данных
 о пользователе одной строкой.
 """
+
+
+def data(name, surname, years, city, e_mail, telephone):
+    print('|'.join([name, surname, years, city, e_mail, telephone]))
+
+
+name = input('Имя -> ')
+surname = input('Фамилия -> ')
+years = input('Возраст -> ')
+city = input('Город -> ')
+e_mail = input('e-mail -> ')
+telephone = input('Номер телефона -> ')
+
+data(name, surname, years, city, e_mail, telephone)
