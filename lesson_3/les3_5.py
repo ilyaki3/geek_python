f63043ddb83c592f55d3cd480bfe0fd4b2ce6d62
ilyaki3 <aliwe2@yandex.ru>
2020-12-18 04:00:14 +0300

    done

--- lesson_3/les3_5.py
+++ lesson_3/les3_5.py
@@ -8,5 +8,21 @@
 Если специальный символ введен после нескольких чисел, то вначале
 нужно добавить сумму этих чисел к полученной ранее сумме и после
 этого завершить программу.
-
 """
+
+SYMBOLS = {'(', '+', ':', '$', '/', '_', '%', '~', '?', '"', '-',
+           '`', '#', '*', '&', '!', '=', '.', ';', '^', ')', ',', '@'}
+numbers_sum = 0
+ex = True
+
+while ex:
+    user_input = input('Введите числа через пробел (спецсимвол для выхода):\n>>> ')
+    numbers = user_input.split()
+    i = 0
+    while i < len(numbers):
+        if numbers[i] in SYMBOLS:
+            ex = False
+            break
+        numbers_sum += int(numbers[i])
+        i += 1
+    print(numbers_sum)
