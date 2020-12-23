941d2fc3f64bee844b4b84997751969df9af3a69
ilyaki3 <aliwe2@yandex.ru>
2020-12-24 01:16:22 +0300

    done

--- lesson_4/les4_6.py
+++ lesson_4/les4_6.py
@@ -9,4 +9,18 @@
 числа 10 завершаем цикл. Во втором также необходимо предусмотреть условие,
 при котором повторение элементов списка будет прекращено.
 """
+from itertools import (count,
+                       cycle)
 
+x = int(input('Генерация начиная с числа:\n>>> '))
+for i in count(x):
+    print(i)
+    if i > 10:
+        break
+
+y = [1, 2, 3]
+for i in cycle(y):
+    y.append(i)
+    if len(y) > 15:
+        break
+print(y)
